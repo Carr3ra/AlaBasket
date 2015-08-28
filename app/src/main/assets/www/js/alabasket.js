f440d6d32347745944935f11b20e284e2f854656
Carr3ra <jon.jaycee@openmailbox.org>
2015-08-28 18:29:44 +0200

    EHF

--- app/src/main/assets/www/js/alabasket.js
+++ app/src/main/assets/www/js/alabasket.js
@@ -1,3 +1,63 @@
+function euskadi(){
+    $("#araba").hide();
+    $("#euskadi").show();
+    $("#exitBtn").buttonMarkup({theme: 'd'});
+}
+
+function araba(){
+    $("#euskadi").hide();
+    $("#araba").show();
+    $("#exitBtn").buttonMarkup({theme: 'b'});
+}
+
+$(document).on("pageshow", "#clasificacion-eus", function(event) {
+	clasificacionEus();
+});
+
+function clasificacionEus() {
+
+				$.mobile.loading('show', {
+					theme : "d",
+					text : "Cargando datos...",
+					textonly : false,
+					textVisible : true
+				});
+				$("#notifClasi-eus").empty();
+				$("#tablaClasi-eus tbody").empty();
+				datos['tipo']="Resultados";
+				datos['fed']="eus";
+				$.ajax({
+					type : "GET",
+					url : "http://lab.url.ph/scrap.php",
+					data : datos,
+					success : function(data) {
+						$("#tablaClasi-eus tbody").empty();
+						var elements = $(data);
+						//var titulo = elements[0].innerHTML;
+						//$("#tituloClasi-eus").text(titulo);
+						$("#categoria-eus").text(liga);
+						var rows = elements.find("tr");
+						for (var i = 1; i < rows.length; i++) {
+
+							$("#tablaClasi-eus tbody").append(rows[i]);
+						}
+						$("table.responsive").each(function(i, element) {
+							unsplitTable($(element));
+						});
+						$("table.responsive").each(function(i, element) {
+							splitTable($(element));
+						});
+					},
+					error : function(error) {
+						$("#notifClasi").html(checkConnection());
+					},
+					complete : function() {
+						$.mobile.loading("hide");
+					},
+					dataType : "html",
+				});
+			}
+
 var app = {
 	// Application Constructor
 	initialize : function() {
